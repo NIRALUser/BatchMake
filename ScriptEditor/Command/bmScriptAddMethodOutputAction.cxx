83c30b58911e8015bdc0f698661e55dd5d80bf12
Julien Jomier <julien.jomier@kitware.com>
2006-06-30 10:42:30 -0400

    ENH: First checkin

---
+++ ScriptEditor/Command/bmScriptAddMethodOutputAction.cxx
@@ -0,0 +1,146 @@
+/*=========================================================================
+
+  Program:   BatchMake
+  Module:    bmScriptAddMethodOutputAction.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+  Copyright (c) 2005 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+=========================================================================*/
+
+#include "bmScriptAddMethodOutputAction.h"
+#include "HttpRequest.h"
+
+namespace bm {
+
+/** */
+ScriptAddMethodOutputAction::ScriptAddMethodOutputAction()
+: ScriptAction()
+{
+}
+
+/** */
+ScriptAddMethodOutputAction::~ScriptAddMethodOutputAction()
+{
+}
+
+/** */
+bool ScriptAddMethodOutputAction::TestParam(ScriptError* error,int linenumber)
+{
+  if (m_parameters.size() < 3)
+    {
+    error->SetError(MString("No enough parameter for AddMethodOutput"),linenumber);
+    return false;
+    }
+
+  m_manager->SetTestVariable(m_parameters[0]);
+
+  for (unsigned int i=1;i<m_parameters.size();i++)
+    {
+    m_manager->TestConvert(m_parameters[i],linenumber);
+    }
+  return true;
+}
+
+/** */
+MString ScriptAddMethodOutputAction::Help()
+{
+  return "AddMethodOutput(methodVariable ExperimentVariable 'MethodName')";
+}
+
+/** */
+void ScriptAddMethodOutputAction::Execute()
+{
+  m_manager->AddDashboardMethodParameter(m_parameters[0].toChar(),
+                               m_parameters[1].toChar(),
+                               m_parameters[2].toChar(),
+                               true);
+  
+  // Create the experiment on the dashboard
+  std::string url = m_manager->GetDashboardURL();
+  m_progressmanager->AddAction("BMDashboard: Creating Parameter");
+  m_progressmanager->IsRunning();
+
+  HttpRequest m_request;
+  m_request.AddParam("user",m_manager->GetDashboardUser());
+
+  // Get the project name
+  const ScriptActionManager::Dashboard * dashboard = m_manager->GetDashboard();
+  const ScriptActionManager::DashboardExperiment* exp = NULL;
+  const ScriptActionManager::DashboardMethod* meth = NULL;
+  std::vector<ScriptActionManager::DashboardExperiment>::const_iterator it = dashboard->experiments.begin();
+  while(it != dashboard->experiments.end())
+    {
+    std::vector<ScriptActionManager::DashboardMethod>::const_iterator itM = (*it).methods.begin();
+    while(itM != (*it).methods.end())
+      {
+      if(!strcmp((*itM).variable.c_str(),m_parameters[1].toChar()))
+        {
+        exp = &(*it);
+        meth = &(*itM);
+        break;
+        }
+      itM++;
+      }
+    it++;
+    }
+
+   if(!meth)
+    {
+    m_progressmanager->AddError("BMDashboard: Cannot find method");
+    return;
+    }
+
+  if(!exp)
+    {
+    m_progressmanager->AddError("BMDashboard: Cannot find experiment");
+    return;
+    }
+
+  m_request.AddParam("project",exp->project);
+  m_request.AddParam("method","CreateParameter");
+  m_request.AddParam("name",m_parameters[2].toChar());
+  m_request.AddParam("experiment",exp->name);
+
+  m_request.AddParam("methodname",meth->name);
+  m_request.AddParam("type","1"); //output
+
+  m_request.AddParam("hostname",m_request.GetHostName());
+  m_request.AddParam("hostip",m_request.GetHostIp());
+
+  url += "/dashboard.php";
+  MString m_output = m_request.Send(url.c_str());
+
+  if (m_output.length()>3)
+    {
+    m_progressmanager->AddError("Bad Host or connexion problem");
+    }
+  else
+    {
+    if (m_output.toInt() == 0)
+      {
+      m_progressmanager->FinishAction(MString("Data sent"));
+      }
+    else
+      {
+      m_progressmanager->FinishAction(MString("Dashboard problem when sending data"));
+      switch(m_output.toInt())
+        {
+        case 1 :  m_progressmanager->AddError("Bad user name"); break;
+        case 2 :  m_progressmanager->AddError("Bad project name"); break;
+        case 3 :  m_progressmanager->AddError("User doesn't belong to project"); break;
+        case 4 :  m_progressmanager->AddError("Over quota: please use DbClear function first"); break;
+        case 5 :  m_progressmanager->AddError("Host Database error"); break;
+        case -1 : m_progressmanager->AddError("Connexion problem"); break;
+        }  
+      }
+    }
+
+}
+
+} // end namespace bm
---
+++ ScriptEditor/Command/bmScriptCreateExperimentAction.cxx
@@ -0,0 +1,111 @@
+/*=========================================================================
+
+  Program:   BatchMake
+  Module:    bmScriptCreateExperimentAction.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+  Copyright (c) 2005 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+=========================================================================*/
+
+#include "bmScriptCreateExperimentAction.h"
+#include "HttpRequest.h"
+
+namespace bm {
+
+/** */
+ScriptCreateExperimentAction::ScriptCreateExperimentAction()
+: ScriptAction()
+{
+}
+
+/** */
+ScriptCreateExperimentAction::~ScriptCreateExperimentAction()
+{
+}
+
+/** */
+bool ScriptCreateExperimentAction::TestParam(ScriptError* error,int linenumber)
+{
+  if (m_parameters.size()< 3)
+    {
+    error->SetError(MString("No enough parameter for CreateExperiment"),linenumber);
+    return false;
+    }
+
+  m_manager->SetTestVariable(m_parameters[0]);
+
+  for (unsigned int i=1;i<m_parameters.size();i++)
+    {
+    m_manager->TestConvert(m_parameters[i],linenumber);
+    }
+  return true;
+}
+
+/** */
+MString ScriptCreateExperimentAction::Help()
+{
+  return "CreateExperiment(var 'Project Name' 'Experiment Name' [description])";
+}
+
+/** */
+void ScriptCreateExperimentAction::Execute()
+{
+  // Add the experiment to the dashboard
+  m_manager->AddDashboardExperiment(m_parameters[0].toChar(),
+                                    m_parameters[1].toChar(),
+                                    m_parameters[2].toChar());
+
+
+  // Create the experiment on the dashboard
+  std::string url = m_manager->GetDashboardURL();
+  m_progressmanager->AddAction("BMDashboard: Creating Experiment");
+  m_progressmanager->IsRunning();
+
+  HttpRequest m_request;
+  m_request.AddParam("user",m_manager->GetDashboardUser());
+  m_request.AddParam("project",m_parameters[1].toChar());
+  m_request.AddParam("method","CreateExperiment");
+  m_request.AddParam("name",m_parameters[2].toChar());
+  if(m_parameters.size()>3)
+    {
+    m_request.AddParam("description",m_parameters[3].toChar());
+    }
+  m_request.AddParam("hostname",m_request.GetHostName());
+  m_request.AddParam("hostip",m_request.GetHostIp());
+
+  url += "/dashboard.php";
+  MString m_output = m_request.Send(url.c_str());
+ 
+  if (m_output.length()>3)
+    {
+    m_progressmanager->AddError("Bad Host or connexion problem");
+    }
+  else
+    {
+    if (m_output.toInt() == 0)
+      {
+      m_progressmanager->FinishAction(MString("Data sent"));
+      }
+    else
+      {
+      m_progressmanager->FinishAction(MString("Dashboard problem when sending data"));
+      switch(m_output.toInt())
+        {
+        case 1 :  m_progressmanager->AddError("Bad user name"); break;
+        case 2 :  m_progressmanager->AddError("Bad project name"); break;
+        case 3 :  m_progressmanager->AddError("User doesn't belong to project"); break;
+        case 4 :  m_progressmanager->AddError("Over quota: please use DbClear function first"); break;
+        case 5 :  m_progressmanager->AddError("Host Database error"); break;
+        case -1 : m_progressmanager->AddError("Connexion problem"); break;
+        }  
+      }
+    }
+}
+
+} // end namespace bm
