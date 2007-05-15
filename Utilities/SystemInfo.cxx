12911ba44ab8714749e9a9016c341244b8aca38e
Julien Jomier <julien.jomier@kitware.com>
2007-05-15 10:51:01 -0400

    BUG: Wrong number of processors

--- Utilities/SystemInfo.cxx
+++ Utilities/SystemInfo.cxx
@@ -1680,7 +1680,7 @@ int SystemInfo::RetreiveInformationFromCpuInfoFile()
   FILE *fd = fopen("/proc/cpuinfo", "r" );
   if ( !fd ) 
     {
-    std::cout << "Problem opening /proc/meminfo" << std::endl;
+    std::cout << "Problem opening /proc/cpuinfo" << std::endl;
     return 0;
     }
   
@@ -1695,12 +1695,12 @@ int SystemInfo::RetreiveInformationFromCpuInfoFile()
   buffer.resize(fileSize-2);
 
   // Number of CPUs
-  long int pos = buffer.find("processor");
+  long int pos = buffer.find("processor     ");
   while(pos != -1)
     {
     m_NumberOfLogicalCPU++;
     m_NumberOfPhysicalCPU++;
-    pos = buffer.find("processor",pos+1);
+    pos = buffer.find("processor     ",pos+1);
     }
 
   // Count the number of physical ids that are the same
