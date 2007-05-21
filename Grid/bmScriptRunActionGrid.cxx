87f1e81a9a8e0c4f691ff82cdffed32d03a9fb15
Julien Jomier <julien.jomier@kitware.com>
2007-05-21 19:40:54 -0400

    ENH: Fixes for condor

--- Grid/bmScriptRunActionGrid.cxx
+++ Grid/bmScriptRunActionGrid.cxx
@@ -28,6 +28,14 @@ void ScriptRunAction::GenerateGrid(const char* appname)
   appvar = appvar.removeChar('{');
   appvar = appvar.removeChar('}');
 
+  if(m_Manager->GetVariable(appvar).size()==0)
+    {
+    std::cout << "ScriptRunAction::GenerateGrid():" 
+              << "Applications should be wrapped to be used on the grid"
+              << std::endl;
+    return;
+    }
+
   // second we try to find the current variable in the list of applications
   ApplicationWrapper * application = NULL;
   MString appName = m_Manager->GetVariable(appvar)[0];
@@ -50,59 +58,65 @@ void ScriptRunAction::GenerateGrid(const char* appname)
     return;
     }
 
-  // We force the application to write the output out as XML
-  std::string xmloutputname = application->GetName().toChar();
-  xmloutputname += ".";
-  char* num = new char[10];
-  sprintf(num,"%d",m_GridModule->GetNumberOfApplications());
-  xmloutputname += num;
-  xmloutputname += ".out.xml";
-  application->SetParameterValue("GenerateXMLFile","","1");
-  application->SetParameterValue("GenerateXMLFile.GenerateXMLFile","",xmloutputname);
-  delete [] num;
-
-  m_GridModule->AddApplication(application);
-
-  // We create the bmGridStore application and send it to condor
-  ApplicationWrapper app;
-  MString gsName = "bmGridStore";
-  bool gsFound = false;
-  ScriptActionManager::ApplicationWrapperListType::iterator itApp = m_Manager->GetApplicationWrapperList()->begin();
-  while (itApp != m_Manager->GetApplicationWrapperList()->end())
+  if(!application->ParameterExists("GenerateXMLFile"))
     {
-    if(!strcmp((*itApp)->GetName().toChar(),gsName.toChar()))
+    m_GridModule->AddApplication(application);
+    }
+  else
+    {
+    // If the application support it. we force the application to write the output out as XML
+    std::string xmloutputname = application->GetName().toChar();
+    xmloutputname += ".";
+    char* num = new char[10];
+    sprintf(num,"%d",m_GridModule->GetNumberOfApplications());
+    xmloutputname += num;
+    xmloutputname += ".out.xml";
+    application->SetParameterValue("GenerateXMLFile","","1");
+    application->SetParameterValue("GenerateXMLFile.GenerateXMLFile","",xmloutputname);
+    delete [] num;
+
+    m_GridModule->AddApplication(application);
+
+    // We create the bmGridStore application and send it to condor
+    ApplicationWrapper app;
+    MString gsName = "bmGridStore";
+    bool gsFound = false;
+    ScriptActionManager::ApplicationWrapperListType::iterator itApp = m_Manager->GetApplicationWrapperList()->begin();
+    while (itApp != m_Manager->GetApplicationWrapperList()->end())
       {
-      app = *(*itApp);
-      gsFound = true;
-      break;
+      if(!strcmp((*itApp)->GetName().toChar(),gsName.toChar()))
+        {
+        app = *(*itApp);
+        gsFound = true;
+        break;
+        }
+      itApp++;
       }
-    itApp++;
+
+    if(!gsFound)
+      {
+      std::cout << "ScriptDashboardSendAction::GenerateCondor : Cannot find bmGridStore " 
+                << appName.toChar() << std::endl;
+      return;
+      }
+
+    app.SetParameterValue("filename","",m_GridModule->GetCurrentScopeFile());
+    app.SetParameterValue("name","","\" \"");
+    app.SetParameterValue("value","","\" \"");
+    //std::string parse = "-p ";
+    //parse += xmloutputname;
+    app.SetParameterValue("parse","","1");
+    app.SetParameterValue("parse.parse","",xmloutputname.c_str());
+    //std::string appvarname = "-a ";
+    MString appn = m_Parameters[1];
+    appn = appn.removeChar('$');
+    appn = appn.removeChar('{');
+    appn = appn.removeChar('}');
+    //appvarname += appn.toChar();
+    app.SetParameterValue("appname","","1");
+    app.SetParameterValue("appname.appname","",appn.toChar());
+    m_GridModule->AddApplication(&app);
     }
-
-  if(!gsFound)
-    {
-    std::cout << "ScriptDashboardSendAction::GenerateCondor : Cannot find bmGridStore " 
-              << appName.toChar() << std::endl;
-    return;
-    }
-
-  app.SetParameterValue("filename","",m_GridModule->GetCurrentScopeFile());
-  app.SetParameterValue("name","","\" \"");
-  app.SetParameterValue("value","","\" \"");
-  //std::string parse = "-p ";
-  //parse += xmloutputname;
-  app.SetParameterValue("parse","","1");
-  app.SetParameterValue("parse.parse","",xmloutputname.c_str());
-  //std::string appvarname = "-a ";
-  MString appn = m_Parameters[1];
-  appn = appn.removeChar('$');
-  appn = appn.removeChar('{');
-  appn = appn.removeChar('}');
-  //appvarname += appn.toChar();
-  app.SetParameterValue("appname","","1");
-  app.SetParameterValue("appname.appname","",appn.toChar());
-
-  m_GridModule->AddApplication(&app);
   
 }
 
