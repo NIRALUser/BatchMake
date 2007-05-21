9bb33b3cd17ce94f40090c720cb2b1b2ee1798c2
Julien Jomier <julien.jomier@kitware.com>
2007-05-21 19:39:30 -0400

    ENH: First checkin

---
+++ Dashboard/bmScriptDashboardKeyAction.cxx
@@ -0,0 +1,61 @@
+/*=========================================================================
+
+  Program:   BatchMake
+  Module:    bmScriptDashboardKeyAction.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+  Copyright (c) 2005 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+=========================================================================*/
+
+#include "bmScriptDashboardKeyAction.h"
+
+namespace bm {
+
+/** */
+ScriptDashboardKeyAction::ScriptDashboardKeyAction()
+: ScriptAction()
+{
+}
+
+/** */
+ScriptDashboardKeyAction::~ScriptDashboardKeyAction()
+{
+}
+
+/** */
+bool ScriptDashboardKeyAction::TestParam(ScriptError* error,int linenumber)
+{
+  if (m_Parameters.size() <1)
+    {
+    error->SetError(MString("No enough parameter for DashboardKey"),linenumber);
+    return false;
+    }
+
+  m_Manager->SetTestVariable(m_Parameters[0]);
+
+  for (unsigned int i=1;i<m_Parameters.size();i++)
+    {
+    m_Manager->TestConvert(m_Parameters[i],linenumber);
+    }
+  return true;
+}
+
+/** */
+MString ScriptDashboardKeyAction::Help()
+{
+  return "DashboardKey('Password')";
+}
+
+/** */
+void ScriptDashboardKeyAction::Execute()
+{
+  m_Manager->SetDashboardKey(m_Parameters[0].toChar());
+}
+
+} // end namespace bm
