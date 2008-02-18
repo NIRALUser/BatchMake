d97f716739c1602e18e330b6acd449907e8111f4
Julien Jomier <julien.jomier@kitware.com>
2008-02-18 14:02:55 -0500

    ENH: First checkin

---
+++ Utilities/ModuleDescriptionParser/BatchMakeUtilities.h
@@ -0,0 +1,24 @@
+/*=========================================================================
+
+  Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   Module Description Parser
+  Module:    $HeadURL: http://www.na-mic.org/svn/Slicer3/trunk/Libs/ModuleDescriptionParser/ModuleDescription.h $
+  Date:      $Date$
+  Version:   $Revision$
+
+==========================================================================*/
+
+#ifndef __BatchMakeUtilities_h
+#define __BatchMakeUtilities_h
+
+#include "ModuleDescriptionParserWin32Header.h"
+
+#include "ModuleDescription.h"
+
+ModuleDescriptionParser_EXPORT std::string GenerateBatchMakeWrapper(const ModuleDescription &module);
+
+#endif
